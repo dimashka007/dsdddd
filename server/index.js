71c57f0842728f3d203b15b3701144e41b7b0d4b
Unknown <demilotion007@gmail.com>
2019-05-31 10:08:21 +0300

    1 port implemeentation

--- server/index.js
+++ server/index.js
@@ -56,6 +56,13 @@ app.post('/upload', upload.single('file'), (req, res) => {
     res.json({file: req.file})
 }) ;
 
+if(process.env.NODE_ENV === 'production'){
+    // stativ folder
+    app.use(express.static(path.join(__dirname, '../client/dist/')))
+
+    app.get(/.*/, (req,res) => res.sendFile(path.join(__dirname, '../client/dist/index.html')));
+}
+
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`server started on port ${port}`));
